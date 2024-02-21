0b374a4a963c1533342affeae5fd0b9bb9332135
Eira Melé <eira.mele@gmail.com>
2024-02-21 12:59:08 +0100

    Show cart amount only if is not empty

--- src/components/common/CartIcon/cart.css/index.tsx
+++ src/components/common/CartIcon/cart.css/index.tsx
@@ -1,21 +1,23 @@
 import { useContext } from "react";
 import "./cart.css";
 import { CartContext } from "../../../../context";
+import { Link } from "react-router-dom";
 
 export default function CartIcon() {
   const context = useContext(CartContext);
   if (context) {
     const { cart } = context;
-    const totalQuantity = cart.reduce((acc, cake) => acc + cake.quantity, 0)
-    console.log(cart)
-    
-    
+    const totalQuantity = cart.reduce((acc, cake) => acc + cake.quantity, 0);
 
     return (
-      <div className="icon-container">
-        <img src="src/assets/cart-icon-30.png" />
-        <span className="cart-count">{totalQuantity}</span>
-      </div>
+      <Link to="/checkout">
+        <div className="icon-container">
+          <img src="src/assets/cart-icon-30.png" />
+          {totalQuantity !== 0 && (
+            <span className="cart-count">{totalQuantity}</span>
+          )}
+        </div>
+      </Link>
     );
   }
 }
