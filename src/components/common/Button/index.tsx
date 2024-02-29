540c287f5eaaae5b3a7ba4ff8798ddd5db97b8bf
Eira Melé <eira.mele@gmail.com>
2024-02-29 15:50:20 +0100

    Last changes

--- src/components/common/Button/index.tsx
+++ src/components/common/Button/index.tsx
@@ -1,19 +1,7 @@
-import React, { ReactNode, MouseEvent } from 'react';
-
-interface ButtonProps {
-  className?: string;
-  onClick?: (event: MouseEvent<HTMLButtonElement>) => void; 
-  children: ReactNode
-}
-
-const Button: React.FC<ButtonProps> = ({ className = "", onClick, children }) => {
+export default function Button({ className = "", onClick, children }) {
   return (
-    <button
-      className={className}
-      onClick={onClick ? onClick : undefined} >
+    <button className={className} onClick={onClick}>
       {children}
     </button>
   );
-};
-
-export default Button;
+}
