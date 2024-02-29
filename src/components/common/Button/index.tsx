f466afabb0a73a20786c2e4e0c1999a7fb8c0368
Eira Melé <eira.mele@gmail.com>
2024-02-29 15:37:49 +0100

    Improve typescript

--- src/components/common/Button/index.tsx
+++ src/components/common/Button/index.tsx
@@ -1,10 +1,19 @@
-export default function Button({ className="", onClick, children }) {
-    return (
-      <button
-        className={ className }
-        onClick={onClick}
-      >
-        {children}
-      </button>
-    );
-  }
+import React, { ReactNode, MouseEvent } from 'react';
+
+interface ButtonProps {
+  className?: string;
+  onClick?: (event: MouseEvent<HTMLButtonElement>) => void; 
+  children: ReactNode
+}
+
+const Button: React.FC<ButtonProps> = ({ className = "", onClick, children }) => {
+  return (
+    <button
+      className={className}
+      onClick={onClick ? onClick : undefined} >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
